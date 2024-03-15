ed40923c9da5270557c9582f076f9b86377a35ea
PurvkumarPatel <mjppatel6666@gmail.com>
2024-03-15 21:47:00 +0530

    JS execution

---
+++ 18JSexecute.js
@@ -0,0 +1,9 @@
+// JS execution context (EC)
+// global EC & Functional EC & Eval EC
+// js is single threaded
+// js code run in 2 phase 
+// 1st memory creation phase // space is alloted to variables etc
+// 2nd execution phase
+
+
+// calls stack
